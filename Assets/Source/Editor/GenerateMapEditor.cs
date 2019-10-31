7137d5bdaba519cc7b8e556ae56abcc3de0ae38c
Kalvis <kalvis.kolesnikovs@gmail.com>
2019-10-31 12:52:47 +0200

    Init project

---
+++ Assets/Source/Editor/GenerateMapEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class GenerateMapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if(DrawDefaultInspector())
+        {
+            if(mapGenerator.autoUpdate)
+            {
+                mapGenerator.DrawMapInEditor();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGenerator.DrawMapInEditor();
+        }
+    }
+}
---
+++ Assets/Source/Scripts/Terrain Generation/MapGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode
+    {
+        NoiseMap, ColorMap, MeshMap
+    };
+    public DrawMode drawMode;
+
+    const int mapChunkSize = 241;
+
+    [Range(0,6)]
+    public int levelOfDetail;
+    public float noiseScale;
+    public float lacunarity;
+    [Range(1, 10)]
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float meshHeightMultiplayer;
+    public AnimationCurve meshHeightCurve;
+
+    public Vector2 offset;
+    public int seed = 1;
+
+    public bool autoUpdate;
+    public TerrainType[] regions;
+
+    public void DrawMapInEditor()
+    {
+        MapData mapData = GenerateMapData();
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
+        else if (drawMode == DrawMode.ColorMap)
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+        else if (drawMode == DrawMode.MeshMap)
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplayer, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+    }
+
+    private MapData GenerateMapData()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height) {
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new MapData(noiseMap, colorMap);
+    }
+
+    private void OnValidate()
+    {
+        if (lacunarity < 1) lacunarity = 1;
+        if (octaves < 1) octaves = 1;
+        if (octaves > 20) octaves = 20;
+        if (noiseScale < 0.03f) noiseScale = 0.03f;
+        if (meshHeightMultiplayer < 1) meshHeightMultiplayer = 1;
+
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
+
+public struct MapData
+{
+    public float[,] heightMap;
+    public Color[] colorMap;
+
+    public MapData(float[,] heightMap, Color[] colorMap)
+    {
+        this.heightMap = heightMap;
+        this.colorMap = colorMap;
+    }
+}
